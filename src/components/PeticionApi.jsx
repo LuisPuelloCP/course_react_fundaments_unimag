349fd54ec6c9a7af3d2496aa2d8424aac256ada3
Luis Puello <luiscapupa@gmail.com>
2023-05-13 16:05:27 -0500

    Contador, Peticion Api, Variables

---
+++ src/components/PeticionApi.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PeticionApi = () => {
+
+  return (
+    <div>
+        <h1>PETICION AL API RICK AND MORTY</h1>
+    </div>
+  )
+}
+
+export default PeticionApi
--- src/components/Variables.jsx
+++ src/components/Variables.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
 const Variables = () => {
+    const saludo = "Hello World"
+    const image = "https://picsum.photos/200"
+    const text_alt = "This is a image" 
   return (
-    <div>Variables</div>
+    <div>
+        <h1>{saludo}</h1>
+        <img src={image} alt={text_alt} />
+    </div>
   )
 }
 
